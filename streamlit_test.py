2362f949fcfc3d8bc98c5bb0d3414fdbfb35e8b5
nilu822 <nilanjana.nc2001@gmail.com>
2025-09-08 13:23:24 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -26,7 +26,7 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System')
-movies = pickle.load(open('movie_list.pkl','rb'))
+movies = pickle.load(open('movies_dict.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
 
 movie_list = movies['title'].values
