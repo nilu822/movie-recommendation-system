7e63ace08476fb6f67868454111e46bdc0f3f830
nilu822 <nilanjana.nc2001@gmail.com>
2025-09-08 13:45:03 +0530

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -8,7 +8,7 @@ import requests
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movie_list=sorted(list(enumerate(distance)), reverse=True, key=lambda x: x[1])[1:6]
+    movie_list=sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     recommended_movie_names = []
     
     for i in movie_list:
