b9daab40a9ed43402293a2d9a38f8c0a0fe26363
nilu822 <nilanjana.nc2001@gmail.com>
2025-10-30 17:17:04 +0530

    Fetch movie posters and update recommendations
    Added a function to fetch movie posters and updated the recommendation function to return posters along with movie names.

--- streamlit_test.py
+++ streamlit_test.py
@@ -3,19 +3,25 @@ import pickle
 import pandas as pd
 import requests
 
-
+def fetch_poster(movie_id):
+    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=ca6d9681eebd1b653272f6ad934f5869'.format(movie_id))
+    data=response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movie_list=sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
     recommended_movie_names = []
+    recommendend_movies_posters=[]
     
     for i in movie_list:
         # fetch the movie poster
+        movie_id=movies.iloc[i[0]].movie_id
         recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommendend_movies_posters.append(fetch_poster(movie_id))
 
-    return recommended_movie_names
+    return recommended_movie_names, recommendend_movies_posters
 
 
 st.header('Movie Recommender System')
@@ -28,6 +34,21 @@ selected_movie = st.selectbox(
     movies['title'].values)
 
 if st.button('Show Recommendation'):
-    recommendations=recommend(selected_movie)
-    for i in recommendations:
-        st.write(i)
+    names,posters=recommend(selected_movie)
+    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
