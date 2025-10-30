dcf6af88c10df45c493f307fa77db5f52b220e22
nilu822 <nilanjana.nc2001@gmail.com>
2025-10-30 17:24:28 +0530

    Update beta_columns to columns in Streamlit

--- streamlit_test.py
+++ streamlit_test.py
@@ -35,7 +35,7 @@ selected_movie = st.selectbox(
 
 if st.button('Show Recommendation'):
     names,posters=recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(names[0])
         st.image(posters[0])
